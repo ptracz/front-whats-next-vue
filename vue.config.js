229a09e21fac74918a0f9a70f64e64e4a22ec18e
Dominik Zmudziak <dominik.zmudziak@gis-support.pl>
2021-06-15 17:33:36 +0200

    initial commit

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'pl',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true
+    }
+  }
+}
