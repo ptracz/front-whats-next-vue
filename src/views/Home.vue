df9b6cf6295b1fea334ee7d020905b6619670ace
Dominik Zmudziak <dominik.zmudziak@gis-support.pl>
2021-06-16 17:36:43 +0200

    add small css changes for review

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,9 @@
       <the-navbar/>
       <v-row no-gutters class="justify-space-between">
         <v-spacer></v-spacer>
-        <v-col v-for="(item, index) in array" :key="index" cols="1">
-          <div class="category">
-            <div class="inside">
-              Ikna
-            </div>
+        <v-col v-for="(item, index) in array" :key="index" cols="auto">
+          <div class="category" :style="{'background-color': item['background-color']}">
+            <v-btn fab :color="item.color" @click="changeColor(item)"></v-btn>
           </div>
         </v-col>
         <v-spacer></v-spacer>
@@ -29,18 +27,39 @@ export default {
     TheNavbar
   },
   data: () => ({
-    array: [1, 2, 3, 4]
+    array: [
+      {color: 'rgb(215,17,17)', activeColor: 'gray', isActive: false, 'background-color': 'white'},
+      {color: 'rgb(25,141,37)', activeColor: 'gray', isActive: false, 'background-color': 'white'},
+      {color: 'rgb(38,40,184)', activeColor: 'gray', isActive: false, 'background-color': 'white'},
+      {color: 'rgb(245,191,2)', activeColor: 'gray', isActive: false, 'background-color': 'white'},
+      {color: 'rgb(157,12,224)', activeColor: 'gray', isActive: false, 'background-color': 'white'},
+      {color: 'rgb(0,255,234)', activeColor: 'gray', isActive: false, 'background-color': 'white'}
+    ]
   }),
+  methods: {
+    changeColor(item) {
+      if (item.isActive) {
+        item.color = item.colorCopy;
+        item['background-color'] = 'white'
+        item.isActive = !item.isActive;
+        return;
+      }
+      item.colorCopy = item.color;
+      item['background-color'] = '#4d4d4d'
+      item.color = item.activeColor;
+      item.isActive = !item.isActive;
+
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
 .category {
   z-index: 999;
-  margin-top: -36px;
-  background-color: white;
   border-radius: 50%;
-  width: 64px;
-  height: 64px;
+  margin: -36px 0 0 0;
+  width: 70px;
+  height: 70px;
   display: flex;
   align-items: center;
   justify-content: center;
